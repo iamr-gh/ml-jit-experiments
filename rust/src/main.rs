57fb99e910a99a2b323f9f52a36c3101063b1b18
Ibrahim Musaddequr Rahman <iamr@umich.edu>
2026-01-13 11:08:20 -0500

    fixed recursive descent parser

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -19,41 +19,52 @@ fn parse_exp(s: &String, start: usize) -> (Box<Op>, usize) {
     // no error handling currently
     let mut end = start;
 
-    // base case, variable or constant
     let mut c = s.as_bytes()[end];
     let mut acc = String::from("");
-    while c.is_ascii_alphabetic() && end < s.len() {
+    while c.is_ascii_alphabetic() {
         acc += (c as char).to_string().as_str();
         end += 1;
+        if end >= s.len() {
+            break;
+        }
         c = s.as_bytes()[end];
     }
 
+    let terminal: Box<Op>;
+
+    // first value must be constant or variable
     if acc.len() != 0 {
-        return (Box::new(Op::Variable(acc)), end);
+        terminal = Box::new(Op::Variable(acc));
     } else {
-        while c.is_ascii_digit() && end < s.len() {
+        while c.is_ascii_digit() {
             acc += (c as char).to_string().as_str();
             end += 1;
+            if end >= s.len() {
+                break;
+            }
             c = s.as_bytes()[end];
         }
-        if acc.len() != 0 {
-            let val: f32 = acc.parse().unwrap();
-            return (Box::new(Op::Constant(val)), end);
-        }
+        let val: f32 = acc.parse().unwrap();
+        terminal = Box::new(Op::Constant(val));
+    }
+
+    // base case
+    if end >= s.len() {
+        return (terminal, end);
     }
 
     // general case
-    let (left, left_end) = parse_exp(&s, start);
-    let op_c = s.as_bytes()[left_end];
-    let (right, right_end) = parse_exp(&s, left_end + 1);
+    let op_c = s.as_bytes()[end];
+    let (right, right_end) = parse_exp(&s, end + 1);
 
     // parse op and construct
     let op = match op_c as char {
-        '+' => Op::Add(left, right),
-        '-' => Op::Sub(left, right),
-        '*' => Op::Mul(left, right),
-        '/' => Op::Div(left, right),
-        '^' => Op::Exp(left, right),
+        '+' => Op::Add(terminal, right),
+        '-' => Op::Sub(terminal, right),
+        '*' => Op::Mul(terminal, right),
+        '/' => Op::Div(terminal, right),
+        '^' => Op::Exp(terminal, right), // this is not actually what exp should be, that's a unary
+        // op
         _ => panic!("not supported op"),
     };
 
@@ -61,7 +72,7 @@ fn parse_exp(s: &String, start: usize) -> (Box<Op>, usize) {
 }
 
 fn main() {
-    let (ast, _) = parse_exp(&String::from("1+2+3+4"), 0);
+    let (ast, _) = parse_exp(&String::from("1^x+3555*long"), 0);
     println!("ast: {:?}", ast);
     println!("Hello, world!");
 }
